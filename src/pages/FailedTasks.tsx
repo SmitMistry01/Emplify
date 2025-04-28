f5787775d016683104d5a2c1628d41047d84fc4a
SmitMistry01 <smitmistry36466@gmail.com>
2025-04-28 13:06:05 +0530

    logout:addedf

--- src/pages/FailedTasks.tsx
+++ src/pages/FailedTasks.tsx
@@ -1,4 +1,4 @@
-function FailedTasks() {
+function FailedTasks({data}:any) {
   return (
     <div className="p-10">
         <div className="overflow-x-auto py-5 no-scrollbar">
@@ -6,7 +6,7 @@ function FailedTasks() {
             {/* Task Card 1 */}
             <div className="flex-shrink-0 w-[320px] h-[350px] bg-red-400 rounded-lg shadow-md hover:shadow-lg transition p-5">
               <div className="flex items-end justify-between">
-                <h3 className=" rounded-xl bg-red-500 font-bold p-1 px-4">High</h3>
+                <h3 className=" rounded-xl bg-red-500 font-bold p-1 px-4">{data.tasks[0].category}</h3>
                 <h4 className="p-1">25 March 2025</h4>
               </div>
               <h1 className="text-2xl font-semibold mt-10 mb-10">Task</h1>
